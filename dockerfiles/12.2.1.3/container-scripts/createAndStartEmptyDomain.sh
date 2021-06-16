1a292869d74ca01a89c5bb1bb2af6d8196dae418
Umesh Adhikari <42251216+uadhikari1989@users.noreply.github.com>
2021-06-15 22:31:35 -0500

    Update createAndStartEmptyDomain.sh

--- dockerfiles/12.2.1.3/container-scripts/createAndStartEmptyDomain.sh
+++ dockerfiles/12.2.1.3/container-scripts/createAndStartEmptyDomain.sh
@@ -4,7 +4,6 @@
 #
 #Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
-
 # If AdminServer.log does not exists, container is starting for 1st time
 # So it should start NM and also associate with AdminServer
 # Otherwise, only start NM (container restarted)
